a1deaaf466bd23a349b71c01f1dce226531ad99a
AvidCoder27 <gilireeves@gmail.com>
2025-02-11 22:51:10 -0500

    actually add the stuff to be imported from my custom stuff *sigh*

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -6,24 +6,6 @@ return {
 
   'williamboman/mason-lspconfig.nvim',
   'neovim/nvim-lspconfig',
-  {
-    'simrat39/rust-tools.nvim',
-    -- configure rust tools
-    config = function()
-      local rt = require 'rust-tools'
-
-      rt.setup {
-        server = {
-          on_attach = function(_, bufnr)
-            -- Hover actions
-            vim.keymap.set('n', '<C-space>', rt.hover_actions.hover_actions, { buffer = bufnr })
-            -- Code action groups
-            vim.keymap.set('n', '<Leader>a', rt.code_action_group.code_action_group, { buffer = bufnr })
-          end,
-        },
-      }
-    end,
-  },
   -- Completion framework:
   'hrsh7th/nvim-cmp',
 
@@ -40,4 +22,7 @@ return {
 
   -- vimspector
   'puremourning/vimspector',
+
+  -- float terminal
+  'voldikss/vim-floaterm',
 }
